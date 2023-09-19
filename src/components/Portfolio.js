d93ed11337bcf2ffe7e3ed16bdbcf47fd767b063
Mark Anthony <manthonyamaro@yahoo.com>
2023-09-19 15:59:03 -0500

    updated portfolio projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,7 +10,36 @@ const Portfolio = () => {
       deployedUrl: 'https://limitless-sierra-77455-2b6ced556da9.herokuapp.com/',
       repoUrl: 'https://github.com/MarkAAmaro/Tech-Blog'
     },
-    // ... other projects
+    {
+      title: 'Coding Knowledge Quiz',
+      imageUrl: '/start quiz.png',
+      deployedUrl: 'https://markaamaro.github.io/Coding-Knowledge-Quiz/',
+      repoUrl: 'https://github.com/MarkAAmaro/Coding-Knowledge-Quiz'
+    },
+    {
+      title: 'Dog Park And Go!',
+      imageUrl: '/DogPark.png',
+      deployedUrl: 'https://markaamaro.github.io/Dog-Park-And-Go/',
+      repoUrl: 'https://github.com/MarkAAmaro/Dog-Park-And-Go'
+    },
+    {
+      title: 'Weather Dashboard',
+      imageUrl: '/Dashboard.png',
+      deployedUrl: 'https://markaamaro.github.io/Weather-Dashboard/',
+      repoUrl: 'https://github.com/MarkAAmaro/Weather-Dashboard'
+    },
+    {
+      title: 'JavaScript Password Generator',
+      imageUrl: '/passwordgenerator.png',
+      deployedUrl: 'https://markaamaro.github.io/JavaScript-Password-Generator/',
+      repoUrl: 'https://github.com/MarkAAmaro/JavaScript-Password-Generator'
+    },
+    {
+      title: 'Daily Event Planner',
+      imageUrl: '/Past,Present,Future.png',
+      deployedUrl: 'https://markaamaro.github.io/Daily-Event-Planner/',
+      repoUrl: 'https://github.com/MarkAAmaro/Daily-Event-Planner'
+    }
   ];
 
   return (
